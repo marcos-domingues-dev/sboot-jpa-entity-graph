63d3d69cf1a47be14847b3f7b3d64043f208c87e
Marcos Domingues <marcos.domingues.dev@gmail.com>
2023-06-15 09:29:15 -0300

    Criação do projeto

---
+++ application/src/main/java/br/dev/techtalk/App.java
@@ -0,0 +1,13 @@
+package br.dev.techtalk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
